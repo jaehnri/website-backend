d47adc90d7d79dbcf2e3130ef4d0773ac6b65b27
João Henri <joaohenri@google.com>
2025-04-19 12:52:01 -0300

    Add functionality to get token and get playing song
    Signed-off-by: João Henri <joaohenri@google.com>

--- internal/spotify/playing.go
+++ internal/spotify/playing.go
@@ -1,9 +1,14 @@
 package spotify
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"io"
 	"log"
-	"os"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
 
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -11,11 +16,39 @@ import (
 const (
 	ClientIDEnv     = "CLIENT_ID_ENV"
 	ClientSecretEnv = "CLIENT_SECRET_ENV"
+	RefreshTokenEnv = "REFRESH_TOKEN_ENV"
+
+	TokenEndpoint            = "https://accounts.spotify.com/api/token"
+	CurrentlyPlayingEndpoint = "https://api.spotify.com/v1/me/player/currently-playing"
 )
 
 type Spotify struct {
 	clientID     string
 	clientSecret string
+
+	accessToken  string
+	refreshToken string
+}
+
+// Define the struct to hold the expected JSON response
+type RefreshTokenResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+type CurrentPlayingResponse struct {
+	IsPlaying  bool `json:"is_playing"`
+	ProgressMs int  `json:"progress_ms"`
+	Item       Item `json:"item"`
+}
+
+type Item struct {
+	DurationMs int      `json:"duration_ms"`
+	Name       string   `json:"name"`
+	Artists    []Artist `json:"artists"`
+}
+
+type Artist struct {
+	Name string `json:"name"`
 }
 
 func NewSpotify() *Spotify {
@@ -29,16 +62,113 @@ func NewSpotify() *Spotify {
 		log.Panic("couldn't retrieve client secret")
 	}
 
+	refreshToken, exists := os.LookupEnv(RefreshTokenEnv)
+	if !exists {
+		log.Panic("couldn't retrieve refresh token")
+	}
+
 	return &Spotify{
 		clientID:     clientID,
 		clientSecret: clientSecret,
+		refreshToken: refreshToken,
 	}
+}
+
+func (s *Spotify) refreshAccessToken() {
+	header := []byte(s.clientID + ":" + s.clientSecret)
+	encodedAuthorizationHeader := base64.RawStdEncoding.EncodeToString(header)
+
+	// Create the form data
+	formData := url.Values{}
+	formData.Set("grant_type", "refresh_token")
+	formData.Set("refresh_token", s.refreshToken)
+	encodedFormData := formData.Encode()
+
+	// Create the HTTP request
+	req, err := http.NewRequest("POST", TokenEndpoint, strings.NewReader(encodedFormData))
+	if err != nil {
+		log.Panic("Error creating request:", err)
+		return
+	}
+
+	// Set the headers
+	req.Header.Set("Authorization", "Basic "+encodedAuthorizationHeader)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	// Create an HTTP client
+	client := &http.Client{}
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Print the response status
+	log.Println("Status:", resp.Status)
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic("Error reading response body:", err)
+		return
+	}
+
+	var accessTokenResponse RefreshTokenResponse
+	err = json.Unmarshal(bodyBytes, &accessTokenResponse)
+	if err != nil {
+		log.Panic("Error unmarshaling JSON:", err)
+		return
+	}
+
+	s.accessToken = accessTokenResponse.AccessToken
+	log.Println("Access Token:", s.accessToken)
+}
+
+func (s *Spotify) getCurrentPlaying() {
+	// Create the HTTP request
+	req, err := http.NewRequest("GET", CurrentlyPlayingEndpoint, nil)
+	if err != nil {
+		log.Panic("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Authorization", "Bearer "+s.accessToken)
+
+	// Create an HTTP client
+	client := &http.Client{}
+
+	// Send the request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Print the response status
+	log.Println("Status:", resp.Status)
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panic("Error reading response body:", err)
+		return
+	}
+
+	var currentPlayingResponse CurrentPlayingResponse
+	err = json.Unmarshal(bodyBytes, &currentPlayingResponse)
+	if err != nil {
+		log.Panic("Error unmarshaling JSON:", err)
+		return
+	}
+
+	log.Println("Song playing:", currentPlayingResponse.Item.Name)
+
 }
 
 func LogSpotify(w http.ResponseWriter, r *http.Request) {
 	log.Println("received an HTTP call!")
-	
-	s := NewSpotify()
-	log.Println(s.clientID)
 
+	s := NewSpotify()
+	s.refreshAccessToken()
+	s.getCurrentPlaying()
 }
