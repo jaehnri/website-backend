ca2d01460369a5c4e8d42c547cfb900d0c8815f6
João Henri <joaohenri@google.com>
2025-05-19 00:52:43 -0300

    Minimal example getting stuff from GCS
    Signed-off-by: João Henri <joaohenri@google.com>

--- internal/ideas/gcs.go
+++ internal/ideas/gcs.go
@@ -2,40 +2,56 @@ package ideas
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"log"
+	"os"
 	"strings"
 
 	"cloud.google.com/go/storage"
 )
 
 const (
-	bucketName = "joaohenri-website-test-bucket"
-	objectName = "test.txt"
+	BucketEnv = "GCS_BUCKET_ENV"
+	ObjectEnv = "GCS_OBJECT_ENV"
 )
 
 type IdeasGCSClient struct {
+	bucket    *storage.BucketHandle
+	object    *storage.ObjectHandle
 	gcsClient *storage.Client
 }
 
 func NewIdeasGCSClient() *IdeasGCSClient {
+	bucketName, exists := os.LookupEnv(BucketEnv)
+	if !exists {
+		log.Panic("couldn't retrieve GCS bucket")
+	}
+
+	objectName, exists := os.LookupEnv(ObjectEnv)
+	if !exists {
+		log.Panic("couldn't retrieve GCS object")
+	}
+
 	client, err := storage.NewClient(context.Background())
 	if err != nil {
 		log.Fatalf("Failed to create GCS client: %v", err)
 	}
+
+	bucket := client.Bucket(bucketName)
+	obj := bucket.Object(objectName)
 	return &IdeasGCSClient{
 		gcsClient: client,
+
+		bucket: bucket,
+		object: obj,
 	}
 }
 
+// GetIdeas fetches all the ideas from a single GCS object.
 func (i *IdeasGCSClient) GetIdeas() ([]string, error) {
-	bucket := i.gcsClient.Bucket(bucketName)
-	obj := bucket.Object(objectName)
-
-	rc, err := obj.NewReader(context.Background())
+	rc, err := i.object.NewReader(context.Background())
 	if err != nil {
-		log.Printf("failed to create object reader for %s/%s: %v", bucketName, objectName, err)
+		log.Printf("failed to create object reader for %s/%s: %v", i.bucket.BucketName(), i.object.ObjectName(), err)
 	}
 	defer rc.Close()
 
@@ -44,10 +60,7 @@ func (i *IdeasGCSClient) GetIdeas() ([]string, error) {
 		log.Printf("failed to read object contents: %v", err)
 	}
 
-	fileContent := string(data)
-	fmt.Printf("contents of gs://%s/%s:\n%s\n", bucketName, objectName, string(data))
-
-	return parseIdeas(fileContent), nil
+	return parseIdeas(string(data)), nil
 }
 
 func (i *IdeasGCSClient) PostIdea(idea string) error {
