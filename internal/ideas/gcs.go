e9d748a7670bc48e2ea9972126c2768de282f030
João Henri <joaohenri@google.com>
2025-05-19 00:41:53 -0300

    Add simple GCS connecting string
    Signed-off-by: João Henri <joaohenri@google.com>

--- internal/ideas/gcs.go
+++ internal/ideas/gcs.go
@@ -1,19 +1,61 @@
 package ideas
 
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+
+	"cloud.google.com/go/storage"
+)
+
+const (
+	bucketName = "joaohenri-website-test-bucket"
+	objectName = "test.txt"
+)
+
 type IdeasGCSClient struct {
-	Some bool
+	gcsClient *storage.Client
 }
 
 func NewIdeasGCSClient() *IdeasGCSClient {
+	client, err := storage.NewClient(context.Background())
+	if err != nil {
+		log.Fatalf("Failed to create GCS client: %v", err)
+	}
 	return &IdeasGCSClient{
-		Some: true,
+		gcsClient: client,
 	}
 }
 
 func (i *IdeasGCSClient) GetIdeas() ([]string, error) {
-	return []string{"123", "1234"}, nil
+	bucket := i.gcsClient.Bucket(bucketName)
+	obj := bucket.Object(objectName)
+
+	rc, err := obj.NewReader(context.Background())
+	if err != nil {
+		log.Printf("failed to create object reader for %s/%s: %v", bucketName, objectName, err)
+	}
+	defer rc.Close()
+
+	data, err := io.ReadAll(rc)
+	if err != nil {
+		log.Printf("failed to read object contents: %v", err)
+	}
+
+	fileContent := string(data)
+	fmt.Printf("contents of gs://%s/%s:\n%s\n", bucketName, objectName, string(data))
+
+	return parseIdeas(fileContent), nil
 }
 
 func (i *IdeasGCSClient) PostIdea(idea string) error {
 	return nil
+}
+
+// Since every idea is a single line, parseIdeas receives all ideas in a single
+// string and parses line-by-line.
+func parseIdeas(fileContent string) []string {
+	return strings.Split(fileContent, "\n")
 }
